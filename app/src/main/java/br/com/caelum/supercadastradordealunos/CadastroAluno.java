2bfe58fb25abdca4a3d51d65ee5fc26d065eaacd
Sidney <sindey_silva13@outlook.com>
2017-07-19 21:15:33 -0300

    Menu item

--- app/src/main/java/br/com/caelum/supercadastradordealunos/CadastroAluno.java
+++ app/src/main/java/br/com/caelum/supercadastradordealunos/CadastroAluno.java
@@ -2,6 +2,9 @@ package br.com.caelum.supercadastradordealunos;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RatingBar;
 
@@ -22,7 +25,22 @@ public class CadastroAluno extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_cadastro_alunos);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.menu_salvar){
+            finish();
+            return true;
+        }
+
+        return false;
     }
 }
